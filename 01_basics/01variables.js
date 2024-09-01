efa257b48ca9ff1b296ad5f094503b6c72565db8
Ankush071020 <jangraankush2002@gmail.com>
2024-09-01 08:22:42 +0000

    container config done

---
+++ 01_basics/01variables.js
@@ -0,0 +1,17 @@
+const account_id=353454
+let account_email="ankush@google.com"
+var account_password="Ankush001"
+account_city="Faridabad"
+let account_state
+// account_id=122222
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+ account_email="jangra@google.com"
+ account_password="Ankush002"
+account_city="Bengluru"
+ account_state
+console.table([account_id,account_email,account_password,account_state,account_city]);
+
+ 
