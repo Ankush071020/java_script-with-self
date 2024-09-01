ad6433348bdc285ab2103eead2d2f9f89fefb6fe
Ankush071020 <jangraankush2002@gmail.com>
2024-09-01 10:58:06 +0000

    container config done

--- 01_basics/03conversion_operation.js
+++ 01_basics/03conversion_operation.js
@@ -1,10 +1,10 @@
 let score=undefined
 // let score=true
 
-console.log(typeof score)
+// console.log(typeof score)
 let valueinNumber=Number(score)
-console.log(typeof valueinNumber);
-console.log(valueinNumber);
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
 // "33"=>33
 // "33abc"=>Nan
 // null=>0
@@ -12,11 +12,36 @@ console.log(valueinNumber);
 let is_loggedin=undefined
 let valueinBollean=Boolean(is_loggedin)
 
-console.log(typeof valueinBollean);
-console.log(valueinBollean);
+// console.log(typeof valueinBollean);
+// console.log(valueinBollean);
 // true=>1; false=>0
 // 1=>true;0=>false
 // ""=>false;"ab"=>true
 // null=>false
 // undefined=>false
+
+// ************operations***************
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+str1="Ankush"
+str2=" Jangra"
+// console.log(str1+str2);=>Ankush Jangra
+// console.log("1"+2);=>12
+// console.log(1+"2");=>12
+// console.log(1+3+"2");=>42
+// console.log("1"+2+3);=>123
+// console.log(+true);=>1
+// let x=2
+// let y=++x
+// console.log(x,y);
+let x=2
+let y=x++
+console.log(x,y);
+
+
+
 
---
+++ 01_basics/04comparision.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2===1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log("2">1);
+// It convert string into number
+
+
+// it does not give the expected answer becoz it convert null and undefined sometime in Nan or zero
+// console.log(null>0);
+// console.log(null>=0);
+// console.log(null==0);
+
+console.log("2"===1);
+// In this case it also check datatype
