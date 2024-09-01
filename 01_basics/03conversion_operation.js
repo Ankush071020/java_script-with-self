c29879af7abf0336a8d605789ac44b101c3e8b0f
Ankush071020 <jangraankush2002@gmail.com>
2024-09-01 09:37:49 +0000

    container config done

---
+++ 01_basics/03conversion_operation.js
@@ -0,0 +1,22 @@
+let score=undefined
+// let score=true
+
+console.log(typeof score)
+let valueinNumber=Number(score)
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+// "33"=>33
+// "33abc"=>Nan
+// null=>0
+// undefined=>Nan
+let is_loggedin=undefined
+let valueinBollean=Boolean(is_loggedin)
+
+console.log(typeof valueinBollean);
+console.log(valueinBollean);
+// true=>1; false=>0
+// 1=>true;0=>false
+// ""=>false;"ab"=>true
+// null=>false
+// undefined=>false
+
