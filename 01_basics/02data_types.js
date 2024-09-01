16b2228b89e51a06d34693ed539f9864e84bd6d7
Ankush071020 <jangraankush2002@gmail.com>
2024-09-01 08:21:38 +0000

    container config done

---
+++ 01_basics/02data_types.js
@@ -0,0 +1,15 @@
+"use strict";/*Treat all js code as newer version*/
+// alert(3+3) we are using node js not browers
+/*
+Data types
+1.number=>2 to power 53;
+2.bigint
+3.string=>"Ankush"
+4.bollean=>True/False
+5.Null=>Stand_alonevalue
+6.undefined=>
+7.symbol=>unique
+8.objet    
+*/
+console.log(typeof undefined)/*undefined*/
+console.log(typeof null)/*object*/
