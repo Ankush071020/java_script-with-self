d599e1ca00ba3ef32b8a37448d2b8f3fccc87b98
Ankush071020 <jangraankush2002@gmail.com>
2024-09-02 12:04:02 +0000

    strings

---
+++ 01_basics/ 06String.js
@@ -0,0 +1,32 @@
+
+const namee="Ankush"
+const grade= " A+"
+// console.log(namee + grade);*****old version
+// console.log(`${namee} is the best software developers with ${grade} grade`);
+
+// const gameName="heitachi" ******old version
+const gameName = new String("heitchi");
+// console.log(gameName[4]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.includes("i"));
+// console.log(gameName.concat(" company"));
+// console.log(gameName.endsWith("i"));
+// console.log(gameName.repeat(2));
+// console.log(gameName.slice(-6));
+// console.log(gameName.split(""));
+console.log(gameName.small());
+console.log(gameName.bold());
+// const another_string=gameName.substring(0,5)
+// console.log(another_string);
+// const strn="   Subash   "
+// console.log(strn.trim());
+// console.log(strn.trimEnd());
+// const url="https://verbose-space-computing-machine-69rjxx4gvrggfrggj.github.dev/"
+// console.log(url.replace("verbose-space-computing-machine-69rjxx4gvrggfrggj.","@"));
+
+
+
+
+
