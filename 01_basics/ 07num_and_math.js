ab3c20a831b674a876db93ad35adbc62ca71c192
Ankush071020 <jangraankush2002@gmail.com>
2024-09-02 14:51:34 +0000

    num

---
+++ 01_basics/ 07num_and_math.js
@@ -0,0 +1,26 @@
+// const balence = new Number(40000000);
+// console.log(balence);
+// console.log(balence.toString().split(""));
+// console.log(balence.valueOf());
+// console.log(balence.toFixed(2));
+// console.log(balence.toExponential(2))
+// console.log(balence.toLocaleString('en-In'))
+// const newNumber=123.8556678
+// console.log(newNumber.toPrecision(5));
+// console.log(newNumber.toPrecision(3));
+// console.log(newNumber.toPrecision(2));
+  
+
+// ***********Math*************
+console.log(Math.abs(-4));
+console.log(Math.round(4.4));
+console.log(Math.floor(4.9));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(Math.sqrt(322)));
+console.log(Math.random());
+console.log(Math.round((Math.random())*10)+1);
+const min=14
+const max=49
+const random_num=Math.round((Math.random() * (max-min+1))+min)
+console.log(random_num);
+
