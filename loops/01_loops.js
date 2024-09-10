b99afe3dfe943092237e3e39c9e6635a24484b72
Ankush071020 <jangraankush2002@gmail.com>
2024-09-10 10:48:49 +0000

    loop

---
+++ loops/01_loops.js
@@ -0,0 +1,37 @@
+for (let i = 0; i <= 10; i++) {
+    if (i==5) {
+        console.log("5 is the best number");
+        // break
+        continue
+        
+    }
+    const element = i;
+    console.log(element);
+    
+    
+}
+
+
+// let n = 5;
+// let string = "";
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+
+// console.log(string);
+
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(`Table of ${i} is:`);
+    
+//     for (let j = 1; j <=10 ; j++) {
+//         const element = i+"*"+j+"="+i*j
+//         console.log(element);
+        
+//     }
+    
+// }
